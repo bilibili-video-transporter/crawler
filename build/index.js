7280072a1ab259e696cca6c29837ea5d3627d29f
bryce loskie <bryce@bryces-MacBook-Pro.local>
2021-01-30 15:04:21 +0800

    [fix] fix the project

--- build/index.js
+++
@@ -1,8 +0,0 @@
-import superAgent from "superagent";
-const test = () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    superAgent.get(url).then((res) => {
-        console.log(res);
-    });
-};
-test();
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import superAgent from "superagent";
 const test = () => {
   const url = "https://jsonplaceholder.typicode.com/posts";
   superAgent.get(url).then((res) => {
-    console.log(res);
+    // console.log(res);
+    console.log("ok");
   });
 };
 
 test();
+
+console.log("it works");
