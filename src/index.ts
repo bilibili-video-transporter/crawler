671c43edf94560092756c5fe3ce9537e37d234b2
noodles <noodles@noodlesdeMacBook-Pro.local>
2021-01-30 15:53:22 +0800

    [feat] 工程结构整理

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import superAgent from "superagent";
+// import superAgent from "superagent";
 
-const test = () => {
-  const url = "https://jsonplaceholder.typicode.com/posts";
-  superAgent.get(url).then((res) => {
-    // console.log(res);
-    console.log("ok");
-  });
-};
+// const test = () => {
+//   const url = "https://jsonplaceholder.typicode.com/posts";
+//   superAgent.get(url).then((res) => {
+//     // console.log(res);
+//     console.log("ok");
+//   });
+// };
 
-test();
+// test();
 
-console.log("it works");
+// console.log("it works");
+import Koa from 'koa'; // koa框架
+import Router from 'koa-router'; // koa-router：处理路由
+
+const app = new Koa(); // 新建一个koa应用
+const router = new Router(); // 新建一个路由
+
+router.get('/hello', async (ctx) => { //定义路由以及对应处理
+  ctx.body = 'Hello World!';
+});
+
+app.use(router.routes()); // 加载路由
+
+app.listen(3000); // 此应用会监听3000端口
+
+console.log('Server running on port 3000');
